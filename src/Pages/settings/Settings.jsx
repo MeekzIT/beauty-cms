984f255420aaed91de612810ba5e06c2cfe1ef66
viktorya_ <viktorya.mazmanyan@debifi.com>
2023-11-15 12:21:57 +0400

    addmin settings

--- src/Pages/settings/Settings.jsx
+++ src/Pages/settings/Settings.jsx
@@ -2,7 +2,10 @@ import { Box, Button, Paper, Typography } from "@mui/material";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { getAdmin, getMe } from "../../store/actions/auth-action";
+import { editAdmin, getAdmin, getMe } from "../../store/actions/auth-action";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { TextField } from "@mui/material";
 
 const Settings = () => {
   const dispatch = useDispatch();
@@ -10,12 +13,46 @@ const Settings = () => {
   const me = useSelector((state) => state.auth.admin);
   const myAdmin = useSelector((state) => state.auth.myAdmin);
 
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("required"),
+    email: Yup.string().email("invalid-email").required("required"),
+    password: Yup.string().min(8, "too-short").max(50, "too-long"),
+    confirmPassword: Yup.string().oneOf(
+      [Yup.ref("password"), null],
+      "passwords-much"
+    ),
+  });
+
+  const validationSchemaAdmin = Yup.object().shape({
+    name: Yup.string().required("required"),
+    email: Yup.string().email("invalid-email").required("required"),
+    password: Yup.string().min(8, "too-short").max(50, "too-long"),
+    confirmPassword: Yup.string().oneOf(
+      [Yup.ref("password"), null],
+      "passwords-much"
+    ),
+  });
+
   useEffect(() => {
     dispatch(getMe());
     dispatch(getAdmin());
   }, []);
 
-  console.log(me, myAdmin);
+  const initialValues = {
+    name: me?.name,
+    email: me?.email,
+    password: "",
+    confirmPassword: "",
+  };
+
+  const initialValuesAdmin = {
+    name: myAdmin?.name,
+    email: myAdmin?.email,
+    password: "",
+    confirmPassword: "",
+  };
+
+  console.log(initialValues);
   return (
     <Box
       component={Paper}
@@ -29,13 +66,144 @@ const Settings = () => {
         <Typography gutterBottom variant="h5" component="div">
           Իմ կարգավորումները
         </Typography>
-        <Box>asdasdasd</Box>
+        {!!initialValues?.email && (
+          <Box>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={(values) =>
+                dispatch(editAdmin({ ...values, oldEmail: me.email }))
+              }
+            >
+              {({ errors, touched }) => (
+                <Form>
+                  <div className="services-title"></div>
+                  <Field
+                    as={TextField}
+                    name="name"
+                    label={"Անուն"}
+                    error={touched.name && Boolean(errors.name)}
+                    helperText={touched.name && errors.name}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                  />
+                  <Field
+                    as={TextField}
+                    name="email"
+                    label={"էլեկտրոնային հասցե"}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                  />
+                  <Field
+                    as={TextField}
+                    name="password"
+                    label={"Գաղտնաբառ"}
+                    type="password"
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={touched.password && errors.password}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                  />
+
+                  <Field
+                    as={TextField}
+                    name="confirmPassword"
+                    label={"Հաստատել գաղտնաբառը"}
+                    type="password"
+                    error={
+                      touched.confirmPassword && Boolean(errors.confirmPassword)
+                    }
+                    helperText={
+                      touched.confirmPassword && errors.confirmPassword
+                    }
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                  />
+                  <Button type="submit">{"Խմբագրել"}</Button>
+                </Form>
+              )}
+            </Formik>
+          </Box>
+        )}
       </Box>
       <Box sx={{ display: "flex", flexDirection: "column" }}>
         <Typography gutterBottom variant="h5" component="div">
           Ադմինի կարգավորումներ
         </Typography>
-        <Box>asdasdasd</Box>
+        <Box>
+          {!!initialValuesAdmin?.email && (
+            <Box>
+              <Formik
+                initialValues={initialValuesAdmin}
+                validationSchema={validationSchemaAdmin}
+                onSubmit={(values) =>
+                  dispatch(editAdmin({ ...values, oldEmail: myAdmin.email }))
+                }
+              >
+                {({ errors, touched }) => (
+                  <Form>
+                    <div className="services-title"></div>
+                    <Field
+                      as={TextField}
+                      name="name"
+                      label={"Անուն"}
+                      error={touched.name && Boolean(errors.name)}
+                      helperText={touched.name && errors.name}
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                    />
+                    <Field
+                      as={TextField}
+                      name="email"
+                      label={"էլեկտրոնային հասցե"}
+                      error={touched.email && Boolean(errors.email)}
+                      helperText={touched.email && errors.email}
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                    />
+                    <Field
+                      as={TextField}
+                      name="password"
+                      label={"Գաղտնաբառ"}
+                      type="password"
+                      error={touched.password && Boolean(errors.password)}
+                      helperText={touched.password && errors.password}
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                    />
+
+                    <Field
+                      as={TextField}
+                      name="confirmPassword"
+                      label={"Հաստատել գաղտնաբառը"}
+                      type="password"
+                      error={
+                        touched.confirmPassword &&
+                        Boolean(errors.confirmPassword)
+                      }
+                      helperText={
+                        touched.confirmPassword && errors.confirmPassword
+                      }
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                    />
+                    <Button type="submit">{"Խմբագրել"}</Button>
+                  </Form>
+                )}
+              </Formik>
+            </Box>
+          )}
+        </Box>
       </Box>
     </Box>
   );
--- src/store/actions/auth-action.js
+++ src/store/actions/auth-action.js
@@ -72,6 +72,7 @@ export const logoutAction = () => {
           localStorage.removeItem("isAuth");
           localStorage.removeItem("isSuper");
           localStorage.removeItem("token");
+          localStorage.removeItem("email");
           localStorage.clear();
           window.location.href = "/";
         }
@@ -135,6 +136,55 @@ export const getAdmin = () => {
         });
       })
       .catch((error) => {
+        console.error(error);
+      });
+  };
+};
+
+export const editAdmin = (data) => {
+  return (dispatch) => {
+    axios
+      .post(`${keys.api}/admin/edit`, data, {
+        headers: {
+          Authorization: `Bearer ${keys.token}`,
+        },
+      })
+      .then(function (response) {
+        if (response.data.succes) {
+          Swal.fire({
+            position: "center",
+            iconColor: "#1d37de",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          axios
+            .post(
+              `${keys.api}/admin/logout`,
+              { email: data.email },
+              {
+                headers: {
+                  Authorization: `Bearer ${keys.token}`,
+                },
+              }
+            )
+            .then(function (response) {
+              if (response.data.succes) {
+                // dispatch({ type: SET_AUTH, payload: false });
+                localStorage.removeItem("isAuth");
+                localStorage.removeItem("isSuper");
+                localStorage.removeItem("token");
+                localStorage.removeItem("email");
+                localStorage.clear();
+                window.location.href = "/";
+              }
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+        }
+      })
+      .catch(function (error) {
         console.error(error);
       });
   };
